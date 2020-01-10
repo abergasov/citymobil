7edb6579c6a70775a77782cb2534d37db0ba142d
Alejandro Bergasov <abergasov@gmail.com>
2020-01-10 14:17:46 +0300

    parse data via regexp

--- console/Command/TestCommand.php
+++ console/Command/TestCommand.php
@@ -15,9 +15,40 @@ class TestCommand extends Command {
      * @return int|null
      */
     protected function execute(InputInterface $input, OutputInterface $output) {
-        echo getenv('SAMPLE_PARAM') . PHP_EOL;
-        $tmp = new DraftObject();
-        $tmp->ping();
+        $data = [
+            'worlds1',
+            'sample_worlds1',
+            'next_worlds1',
+            'test_worlds1',
+            'new_worlds1',
+            'again_worlds2',
+            'next_worlds2',
+            'test_worlds2',
+            'new_worlds2',
+            'again_worlds2',
+            'agaain_worlds2',
+        ];
+
+        $prefixArr = ['nex', 'test', 'again'];
+        foreach ($prefixArr as $singlePrefix) {
+            $matches = $this->parseRegexpData($data, $singlePrefix);
+            echo 'pattern:' . $singlePrefix . PHP_EOL;
+            echo 'found matches: ' . count($matches) . PHP_EOL;
+            var_dump($matches);
+            echo PHP_EOL;
+        }
         return 0;
+    }
+
+    private function parseRegexpData (array $words, string $prefix) : array {
+        $result = [];
+        foreach ($words as $singleWorld) {
+
+            if (!preg_match('/' . $prefix . '/', $singleWorld)) {
+                continue;
+            }
+            $result[] = $singleWorld;
+        }
+        return $result;
     }
 }
